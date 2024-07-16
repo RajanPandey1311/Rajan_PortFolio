c113fce18891f57431ca55bc5e5308829d451b50
Rajan Pandey <130823894+RajanPandey1311@users.noreply.github.com>
2024-07-16 18:41:28 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/RAJAN_PANDEY_RESUME_2024.pdf";
+import CV from "../../assets/Rajan_Pandey_Resume_2024.pdf";
 
 const CTA = () => {
   return (
